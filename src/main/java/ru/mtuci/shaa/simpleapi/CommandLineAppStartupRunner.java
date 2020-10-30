6d332695cec0c7fa7eb11ccf04d66582e5727a78
zipa455 <zipa455@gmail.com>
2020-10-30 17:36:28 +0300

    Убрал ненужное

--- src/main/java/ru/mtuci/shaa/simpleapi/CommandLineAppStartupRunner.java
+++ src/main/java/ru/mtuci/shaa/simpleapi/CommandLineAppStartupRunner.java
@@ -1,10 +1,14 @@
 package ru.mtuci.shaa.simpleapi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import ru.mtuci.shaa.simpleapi.dao.SubjectRepository;
 
+import java.util.logging.Logger;
+
+@Slf4j
 @Component
 public class CommandLineAppStartupRunner implements CommandLineRunner {
     private final SubjectRepository subjectRepository;
@@ -16,7 +20,8 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
 
 
     @Override
-    public void run( String...args) throws Exception {
-        System.out.println( subjectRepository.findById( 1L ).get() );
+    public void run( String...args)  {
+        //System.out.println( subjectRepository.findById( 1L ).get() );
+        log.info("Start");
     }
 }
