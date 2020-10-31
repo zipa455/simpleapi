b0762dc0704c3e5b4b37fa9ab7713d53d9d93057
zipa455 <zipa455@gmail.com>
2020-11-01 01:57:57 +0300

    upgrade to java 15

--- src/main/java/ru/mtuci/shaa/simpleapi/SimpleApiApplication.java
+++ src/main/java/ru/mtuci/shaa/simpleapi/SimpleApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleApiApplication {
+public class SimpleapiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SimpleApiApplication.class, args);
+        SpringApplication.run(SimpleapiApplication.class, args);
     }
 
 }
