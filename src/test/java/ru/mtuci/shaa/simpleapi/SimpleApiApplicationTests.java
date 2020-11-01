45f026c9c74a639d2949bcb5938b9000f60b1fed
zipa455 <zipa455@gmail.com>
2020-11-01 23:19:17 +0300

    add test main

--- src/test/java/ru/mtuci/shaa/simpleapi/SimpleApiApplicationTests.java
+++ src/test/java/ru/mtuci/shaa/simpleapi/SimpleApiApplicationTests.java
@@ -1,16 +1,27 @@
 package ru.mtuci.shaa.simpleapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.mtuci.shaa.simpleapi.conroller.StatusController;
+import ru.mtuci.shaa.simpleapi.conroller.SubjectController;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SimpleapiApplicationTests {
 
+    @Autowired
+    StatusController statusController;
+
+    @Autowired
+    SubjectController  subjectController;
+
     @Test
-    void contextLoads() {
-        Integer i = 1;
-        assertNotNull( i );
+    void mainTest() {
+        assertThat(statusController).isNotNull();
+        assertThat(subjectController).isNotNull();
     }
 
 
